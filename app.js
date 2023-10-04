ac2b050af2d904937e69f77221447a16f314e565
jinnycode200 <jinnycode200@gmail.com>
2023-10-04 19:02:47 +0900

    CREATE TABLE

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ const db = mysql.createConnection({
     user: 'root',
     password: 'tuadlek11',
     database: 'wanted_preonboarding_backend',
-    port: 3306
+    port: 3306,
+    multipleStatements : true
 });
 
-db.connect(function(err,result) {
+db.connect(function(err) {
     if(err) throw err;
-    console.log(result);
+    db.query("CREATE TABLE company(idx INT NOT NULL PRIMARY KEY AUTO_INCREMENT,name VARCHAR(11) NOT NULL);CREATE TABLE notice(idx INT NOT NULL PRIMARY KEY AUTO_INCREMENT, nation VARCHAR(11) NOT NULL, region VARCHAR(11) NOT NULL, position VARCHAR(20) NOT NULL, reward INT, skill VARCHAR(11) NOT NULL, detail VARCHAR(200),company_id INT NOT NULL, FOREIGN KEY(company_id) REFERENCES company(idx));CREATE TABLE user(idx INT NOT NULL PRIMARY KEY AUTO_INCREMENT, id VARCHAR(11) NOT NULL,applied_notice INT NOT NULL, FOREIGN KEY(applied_notice) REFERENCES notice(idx));",
+    function(err) {
+        console.log(err)
+    })
 })
